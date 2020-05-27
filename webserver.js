391253fdda3323235a482d920da94e752d64c73a
jwalit21 <jwalishah2q@gmail.com>
2020-05-27 17:00:17 +0530

    basic javascript and node codes

--- webserver.js
+++ webserver.js
@@ -3,3 +3,5 @@ http.createServer(function(req,res){
     res.writeHead(200,{'Content-type':'text/plain'});
     res.end('Hello');
 }).listen(1337,'127.0.0.1');
+const zipcode = require('zipcode');
+console.log(zipcode.lookup('39002'));
